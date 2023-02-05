7bba5448333f64eceb178ba080a1e1df02fd74ec
Levid <jassontejada@gmail.com>
2023-02-05 16:25:13 -0400

    Realizando problema2-2

---
+++ problema2/problema2-2.sql
@@ -0,0 +1,21 @@
+if object_id('libros') is not null
+	drop table libros;
+
+create table libros (
+	titulo varchar(20),
+	autor varchar(30),
+	editorial varchar(15)
+);
+
+exec sp_tables @table_owner='dbo';
+
+exec sp_columns libros;
+
+insert into libros (titulo,autor,editorial)
+values ('El aleph','Borges','Planeta');
+insert into libros (titulo,autor,editorial) 
+values ('Martin Fierro','Jose Hernandez','Emece');
+insert into libros (titulo,autor,editorial)
+values ('Aprenda PHP','Mario Molina','Emece');
+
+select * from libros;
