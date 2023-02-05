32484d04d796de4fa3bc7c9fad71934dcd4f26ab
Levid <jassontejada@gmail.com>
2023-02-05 16:15:30 -0400

    Primer commit p3

---
+++ problema3.sql
@@ -0,0 +1,14 @@
+if object_id('agenda') is not null
+	drop table agenda;
+
+create table agenda(
+	apellido varchar(30),
+	nombre varchar(20),
+	domicilio varchar(30),
+	telefono varchar(11)
+);
+
+exec sp_tables @table_owner='dbo';
+
+exec sp_columns agenda;
+
