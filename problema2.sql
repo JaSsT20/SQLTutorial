af2b5cdd152dd2eec2d3d594ba34ccf72e7c5304
Levid <jassontejada@gmail.com>
2023-02-05 16:03:32 -0400

    Creando tabla libros

--- problema2.sql
+++ problema2.sql
@@ -2,3 +2,9 @@ if object_id('libros') is not null
 	drop table libros;
 
 exec sp_tables @table_owner='dbo'
+
+create table libros(
+	titulo varchar(20),
+	autor varchar(30),
+	editorial varchar(15)
+);
