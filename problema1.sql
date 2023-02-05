35c39eae7bd85cc14b3f15d7404644173d764bb5
Levid <jassontejada@gmail.com>
2023-02-05 15:50:31 -0400

    Creando mal la tabla

--- problema1.sql
+++ problema1.sql
@@ -1,3 +1,10 @@
 if object_id('agenda') is not null
   drop table agenda;
 
+create table /agenda
+(
+	apellido varchar(30),
+	nombre varchar(20),
+	domicilio varchar(30),
+	telefono varchar(11)
+ );
