2fecbbd110ee4d121a2a2d10614ddd603ebf0136
Levid <jassontejada@gmail.com>
2023-02-19 10:44:43 -0400

    Agregando p2

--- problema5/problema5.sql
+++ problema5/problema5.sql
@@ -33,3 +33,34 @@ if object_id('agenda') is not null
 
  select nombre from agenda
   where telefono='4545454';
+
+  --PARTE 2
+
+  if object_id('libros') is not null
+  drop table libros;
+
+ create table libros (
+  titulo varchar(20),
+  autor varchar(30),
+  editorial varchar(15)
+ );
+
+ exec sp_columns libros;
+
+ insert into libros(titulo,autor,editorial) values
+  ('El aleph','Borges','Emece');
+ insert into libros(titulo,autor,editorial) values
+  ('Martin Fierro','Jose Hernandez','Emece');
+ insert into libros(titulo,autor,editorial) values
+  ('Martin Fierro','Jose Hernandez','Planeta');
+ insert into libros(titulo,autor,editorial) values
+  ('Aprenda PHP','Mario Molina','Siglo XXI');
+
+ select * from libros
+  where autor='Borges';
+
+ select titulo from libros
+  where editorial='Emece';
+
+ select editorial from libros
+  where titulo='Martin Fierro';
