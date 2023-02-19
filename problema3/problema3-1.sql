5a34833285ca7f07b00bcb7ec1c27c210ced3693
Levid <jassontejada@gmail.com>
2023-02-19 10:03:29 -0400

    Agregando parte dos

--- problema3/problema3-1.sql
+++ problema3/problema3-1.sql
@@ -20,3 +20,27 @@
   values ('Elsa y Fred','China Zorrilla',110,2);
 
  select * from peliculas;
+
+ --PARTE 2
+
+  if object_id('empleados') is not null
+  drop table empleados;
+
+ create table empleados(
+  nombre varchar(20),
+  documento varchar(8),
+  sexo varchar(1),
+  domicilio varchar(30),
+  sueldobasico float
+ );
+
+ exec sp_columns empleados;
+
+ insert into empleados (nombre, documento, sexo, domicilio, sueldobasico)
+  values ('Juan Perez','22333444','m','Sarmiento 123',500);
+ insert into empleados (nombre, documento, sexo, domicilio, sueldobasico)
+  values ('Ana Acosta','24555666','f','Colon 134',650);
+ insert into empleados (nombre, documento, sexo, domicilio, sueldobasico)
+  values ('Bartolome Barrios','27888999','m','Urquiza 479',800);
+
+ select * from empleados;
